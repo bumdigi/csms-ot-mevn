6f8b1f6ac9426d7a9dd4e245dec56d9458b64fb8
DESKTOP-U1NR87U\LGCNS <aerin.lee@lgcns.com>
2019-11-15 10:39:04 +0900

    change fontsize by range

--- src/components/EditorComponent.vue
+++ src/components/EditorComponent.vue
@@ -11,15 +11,25 @@
               <option value="text/javascript" selected>javascript</option>
               <option value="htmlmixed">HTML</option>
               <option value="css">css</option>
+              <option value="go">Go</option>
+              <option value="text/x-java">Java</option>
+              <option value="text/x-c++src">C++</option>
+              <option value="text/x-csrc">C</option>
             </select>
             <div class="input-group-prepend ml-2">
               <div class="input-group-text" id="btnGroupAddon">Theme</div>
             </div>
-            <select class="form-control custom-select">
-              <option value="" selected>theme1</option>
-              <option value="">theme2</option>
-              <option value="">theme3</option>
+            <select class="form-control custom-select" v-model="cmOptions.theme">
+              <option value="base16-dark" selected>dark</option>
+              <option value="base16-light">light</option>
+              <option value="panda-syntax">panda</option>
+              <option value="monokai">monokai</option>
             </select>
+            <div class="input-group-prepend ml-2">
+               <div class="input-group-text" id="btnGroupAddon">Font Size</div>
+              <input type="range" class="custom-range" min="8" max="30" id="customRange2" value="10" v-on:change="changeFontSize">
+            </div>
+           
           </div>
           <div class="btn-group ml-2" role="group" aria-label="First group">
             <button type="button" class="btn btn-secondary">Compile</button>
@@ -28,7 +38,7 @@
         </div>
       </div>
     </div>
-    <div class="container">
+    <div class="container" id="codeEditor">
       <codemirror ref="myCm" class="code-editor"
                   :value="code"
                   :options="cmOptions"
@@ -45,8 +55,13 @@
 import 'codemirror/mode/javascript/javascript.js'
 import 'codemirror/mode/htmlmixed/htmlmixed.js'
 import 'codemirror/mode/css/css.js'
+import 'codemirror/mode/go/go.js'
+import 'codemirror/mode/clike/clike.js'
 // theme css
 import 'codemirror/theme/base16-dark.css'
+import 'codemirror/theme/base16-light.css'
+import 'codemirror/theme/panda-syntax.css'
+import 'codemirror/theme/monokai.css'
 // more codemirror resources
 // import 'codemirror/some-resource...'
 import { codemirror } from 'vue-codemirror'
@@ -55,7 +70,6 @@ import { codemirror } from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
 
 // require more codemirror resource...
-
 export default {
   data () {
     return {
@@ -80,6 +94,10 @@ export default {
     onCmCodeChange(newCode) {
       console.log('this is new code', newCode)
       this.code = newCode
+    },
+    changeFontSize(){
+      let size = document.getElementById('customRange2').value;
+      document.getElementById('codeEditor').setAttribute('style', 'font-size:'+size+'px');
     }
   },
   computed: {
@@ -96,3 +114,6 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+</style>
