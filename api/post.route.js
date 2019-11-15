eb6ef30cc6bd626f0d3edb7c8616f8cabd4e11c3
daulso <daulso@naver.com>
2019-11-15 10:50:55 +0900

    trying backend

--- api/post.route.js
+++ api/post.route.js
@@ -14,6 +14,17 @@ postRoutes.route('/add').post((req,res) => {
     })
 })
 
+postRoutes.route('/code').post((req,res) => { //
+  let post = new Post(req.body)
+  post.save()
+    .then(() => {
+      res.status(200).json({'business': 'business in added successfully'})
+    })
+    .catch(() => {
+      res.status(400).send('unable to save to database')
+    })
+})
+
 postRoutes.route('/').get((req, res) => {
   Post.find((err, posts) => {
     if (err) {
--- api/server.js
+++ api/server.js
@@ -7,11 +7,24 @@ const mongoose = require('mongoose')
 const config = require('./DB.js')
 const postRoute = require('./post.route')
 
+const runWandbox = require('wandbox-api'); //
+
 mongoose.Promise = global.Promise
 mongoose.connect(config.DB, {useNewUrlParser: true, useUnifiedTopology: true}).then(
   () => {console.log(`Database is connected`)},
   err => {console.log(`Can not connect to the database`)}
 )
+
+//code입력
+runWandbox.fromString('console.log(1+1);',{'compiler':'nodejs-head'},function clbk(error, results){
+	if(error){
+		throw new Error(error.message);
+	}
+	var out=results;
+	
+	var response = JSON.parse(JSON.stringify(out));
+	console.log(response['program_output']);
+});
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
